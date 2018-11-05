74de7512c3834dad06ba544bf117045c5c2744f7
Ivan Bonk <vaaeking@gmail.com>
2018-11-05 16:57:10 +0000

    bootstrap test

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 750);
+        }
+    });
+});
+
+const wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+})
+wow.init();
